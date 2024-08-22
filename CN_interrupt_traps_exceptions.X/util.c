4efe8c51284aadd00d93ec1a6549e1b1e9fff259
sergehould <houlds@vaniercollege.qc.ca>
2024-08-22 16:47:19 -0400

    Update util.c

--- CN_interrupt_traps_exceptions.X/util.c
+++ CN_interrupt_traps_exceptions.X/util.c
@@ -426,39 +426,40 @@ void reverse(char str[], int length) {
 }
 
 // Implementation of itoa()
-char* itoa(int num, char* str, int base){
-    // int i = 0;
-    // bool isNegative = false;
+char* itoa(int num, char* str, int _base){
+    int i = 0;
+    bool isNegative = false;
 
-    // // Handle 0 explicitly, otherwise empty string is printed for 0
-    // if (num == 0) {
-        // str[i++] = '0';
-        // str[i] = '\0';
-        // return str;
-    // }
+    // Handle 0 explicitly, otherwise empty string is printed for 0
+    if (num == 0) {
+        str[i++] = '0';
+        str[i] = '\0';
+        return str;
+    }
 
-    // // In standard itoa(), negative numbers are handled only with base 10.
-    // // Otherwise, numbers are considered unsigned.
-    // if (num < 0 && base == 10) {
-        // isNegative = true;
-        // num = -num;
-    // }
+    // In standard itoa(), negative numbers are handled only with base 10.
+    // Otherwise, numbers are considered unsigned.
+    if (num < 0 && _base == 10) {
+        isNegative = true;
+        num = -num;
+    }
 
-    // // Process individual digits
-    // while (num != 0) {
-        // int rem = num % base;
-        // str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
-        // num = num / base;
-    // }
+    // Process individual digits
+    while (num != 0) {
+        int rem = num % _base;
+        str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
+        num = num / _base;
+    }
 
-    // // If number is negative, append '-'
-    // if (isNegative)
-        // str[i++] = '-';
+    // If number is negative, append '-'
+    if (isNegative)
+        str[i++] = '-';
 
-    // str[i] = '\0'; // Null-terminate string
+    str[i] = '\0'; // Null-terminate string
 
-    // // Reverse the string
-    // reverse(str, i);
+    // Reverse the string
+    reverse(str, i);
 
-    // return str;   
-// }
+    return str;   
+}
+
